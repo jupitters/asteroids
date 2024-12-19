ec4fef8422bd05b3aaf09ae064f3369ddaab18ba
jupitters <sampaiobale@gmail.com>
2024-12-19 11:24:23 -0300

    add constants and created the window

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-import pygame
+import pygame as pg
+from constants import *
 
 def main():
-    print("Iniciando Asteroids!")
+    pg.init()
+    screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while(True):
+        pg.Surface.fill(screen, (0,0,0))
+        pg.display.flip()
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+    
 
 if __name__ == "__main__":
     main()
